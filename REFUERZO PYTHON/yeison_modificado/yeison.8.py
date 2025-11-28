13fa2f468d3fc9dc6c37217436d546cf4b30586c
dayeison87 <dayeison87@gmail.com>
2025-11-28 17:12:48 -0500

    listi

--- REFUERZO PYTHON/yeison_modificado/yeison.8.py
+++ REFUERZO PYTHON/yeison_modificado/yeison.8.py
@@ -11,4 +11,4 @@ print(concatenar_cadenas(["Hola","moreno ","yeison"]))
 def lista_impares(yeison_david_moreno_n):
     return [x for x in range(1, yeison_david_moreno_n+1) if x % 2 != 0]
 
-print(lista_impares(10)) q.enqueue(2); print(q.dequeue())
+print(lista_impares(10)) .enqueue(2); print(q.dequeue())
